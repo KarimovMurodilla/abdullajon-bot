6edbe7aeee957751fc413b7aba52f897dbdfe951
KarimovMurodilla <karimovmurodilla15@gmail.com>
2024-09-05 11:33:37 +0500

    tts migrated to microsoft azure

--- src/bot/utils/tts.py
+++ src/bot/utils/tts.py
@@ -5,18 +5,29 @@ from src.configuration import conf
 
 class Tts:
     def __init__(self):
-        self.url = "https://api.muxlisa.uz/v1/api/services/tts/"
-        self.token = conf.TTS_TOKEN
-        self.speaker_id = 1
+        region = 'eastus'  # Например, 'eastus'
+        self.url = f'https://{region}.tts.speech.microsoft.com/cognitiveservices/v1'
+        self.speaker_id = 0
 
     def text_to_speech(self, text: str, file_path: str) -> str:
-        payload = f"token={self.token}&text={text}&speaker_id={self.speaker_id}"
-
+        # Заголовки
         headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Ocp-Apim-Subscription-Key': conf.TTS_TOKEN,
+            'Content-Type': 'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'audio-16khz-32kbitrate-mono-mp3'
         }
 
-        response = requests.post(self.url, headers=headers, data=payload)
+        # Тело запроса в формате SSML (Speech Synthesis Markup Language)
+        ssml = f"""
+            <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="uz-UZ">
+                <voice name="uz-UZ-MadinaNeural">
+                    {text}
+                </voice>
+            </speak>
+        """
+
+        # Запрос к API
+        response = requests.post(self.url, headers=headers, data=ssml)
 
         with open(file_path, "wb") as f:
             f.write(response.content)
